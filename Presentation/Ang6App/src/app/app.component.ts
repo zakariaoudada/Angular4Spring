52522afdc9d8aca42030bc9c62e245cd55a35422
zakaria <zakaria.oudada@gmail.com>
2018-09-24 22:14:11 +0100

    add object and show it in view

--- Presentation/Ang6App/src/app/app.component.ts
+++ Presentation/Ang6App/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ang6App';
+   contact = {
+    name : 'zakaria',
+    tel: '0657785970'
+  };
 }
