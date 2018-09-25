474cd9d0790190540a38104e8f09ac8408111bd0
zakaria <zakaria.oudada@gmail.com>
2018-09-25 22:45:10 +0100

    generation deux components et inclusion bootstarp [ng g c ] [ npm install bootstrap ]

--- Presentation/Ang6App/src/app/app.module.ts
+++ Presentation/Ang6App/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule
